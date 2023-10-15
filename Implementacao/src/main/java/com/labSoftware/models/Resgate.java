063bc54f459075898cd49264216d8de4e69ef7c3
Eric Diniz <64018893+ericdiniz@users.noreply.github.com>
2023-10-15 19:44:14 -0300

    update resgate

---
+++ Implementacao/src/main/java/com/labSoftware/models/Resgate.java
@@ -0,0 +1,41 @@
+package com.labSoftware.models;
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+
+@Entity
+public class Resgate {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_resgate", unique = true)
+    private Long id;
+
+    @Column(name = "description")
+    private String description;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_professor")
+    private Professor professor;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_aluno")
+    private Aluno aluno;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id")
+    private Vantagem vantagem;
+
+    @Temporal(TemporalType.TIMESTAMP) // Isso indica que o campo é uma data e hora
+    @Column(name = "timestamp")
+    private Date timestamp;
+}
