e5d7d7bc1b8041a5519d0cc524108020f455661c
Eric Diniz <64018893+ericdiniz@users.noreply.github.com>
2023-10-19 20:23:39 -0300

    generic table + path para extrato professor

---
+++ Implementacao/view/src/components/generic-table/generic-table.component.jsx
@@ -0,0 +1,55 @@
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import { styled } from "@mui/material/styles";
+import * as React from "react";
+
+const Container = styled(Box)({
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  minHeight: "100vh",
+});
+
+const StyledTableContainer = styled(TableContainer)({
+  maxWidth: "1000px",
+  width: "100%",
+});
+
+const StyledTable = styled(Table)({
+  width: "100%", // A tabela ocupará 100% da largura do TableContainer
+});
+
+const GenericTable = ({ headers, data }) => {
+  return (
+    <Container>
+      <StyledTableContainer component={Paper}>
+        <StyledTable sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              {headers.map((header, index) => (
+                <TableCell key={index}>{header}</TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {data.map((row, rowIndex) => (
+              <TableRow key={rowIndex}>
+                {headers.map((header, colIndex) => (
+                  <TableCell key={colIndex}>{row[header]}</TableCell>
+                ))}
+              </TableRow>
+            ))}
+          </TableBody>
+        </StyledTable>
+      </StyledTableContainer>
+    </Container>
+  );
+};
+
+export default GenericTable;
--- Implementacao/view/src/routers/all-routes.jsx
+++ Implementacao/view/src/routers/all-routes.jsx
@@ -1,17 +1,21 @@
-import NotFoundPage from "../pages/not-found/notfound.page";
-import { Route,Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import CadastroUsuarioPage from "../pages/cadastro-usuario/cadastro-usuario.page";
+import ExtratoProfessor from "../pages/extrato-by-id-professor/extrato-by-id-professor.page";
 import HomePage from "../pages/home/home.page";
 import LoginPage from "../pages/login/login.page";
-import CadastroUsuarioPage from "../pages/cadastro-usuario/cadastro-usuario.page";
-
+import NotFoundPage from "../pages/not-found/notfound.page";
 
 const AllRoutes = () => {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<HomePage/>}></Route>
-        <Route path="auth" element={<LoginPage/>}></Route>
-        <Route path="register" element={<CadastroUsuarioPage/>}></Route>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="auth" element={<LoginPage />}></Route>
+        <Route path="register" element={<CadastroUsuarioPage />}></Route>
+        <Route
+          path="professor/extratoProfessor"
+          element={<ExtratoProfessor />}
+        ></Route>
         <Route path="*" element={<NotFoundPage />}></Route>
       </Routes>
     </div>
